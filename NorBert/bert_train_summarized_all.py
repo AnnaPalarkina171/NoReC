0530553fb4556afbccbfe5a0011f26dafce3cf0b
Anna Palatkina <31884615+AnnaPalarkina171@users.noreply.github.com>
2022-06-13 13:19:05 +0200

    Add files via upload

---
+++ NorBert/bert_train_summarized_all.py
@@ -0,0 +1,199 @@
+import transformers
+from transformers import BertModel, BertTokenizer, AdamW, get_linear_schedule_with_warmup, AutoTokenizer, LongformerTokenizer
+import torch
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+from collections import defaultdict
+from textwrap import wrap
+from torch import nn, optim
+from torch.utils.data import Dataset, DataLoader
+from tqdm.auto import tqdm
+import torch.nn.functional as F
+import warnings
+warnings.filterwarnings("ignore")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+print('NorBert on summarized texts (whole dataset - old summarization)')
+
+PRE_TRAINED_MODEL_NAME = 'ltgoslo/norbert2'
+MAX_LEN = 512   
+RANDOM_SEED = 42
+BATCH_SIZE =4
+EPOCHS = 20
+
+df = pd.read_csv('summarized_data_old.csv')
+class_names = df.label.unique()
+tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
+
+class Dataset(Dataset):
+  def __init__(self, texts, targets, tokenizer, max_len):
+    self.texts = texts
+    self.targets = targets
+    self.tokenizer = tokenizer
+    self.max_len = max_len
+
+  def __len__(self):
+    return len(self.texts)
+
+  def __getitem__(self, item):
+    text = str(self.texts[item])
+    target = self.targets[item]
+    encoding = self.tokenizer.encode_plus(
+      text,
+      add_special_tokens=True,
+      max_length=self.max_len,
+      return_token_type_ids=False,
+      pad_to_max_length=True,
+      return_attention_mask=True,
+      truncation=True,
+      return_tensors='pt',
+    )
+    return {
+      'text': text,
+      'input_ids': encoding['input_ids'].flatten(),
+      'attention_mask': encoding['attention_mask'].flatten(),
+      'targets': torch.tensor(target, dtype=torch.long)
+    }
+
+
+def create_data_loader(df, tokenizer, max_len, batch_size):
+  ds = Dataset(
+    texts=df.text.to_numpy(),
+    targets=df.label.to_numpy(),
+    tokenizer=tokenizer,
+    max_len=max_len
+  )
+  return DataLoader(
+    ds,
+    batch_size=batch_size
+  )
+
+
+class SentimentClassifier(nn.Module):
+
+  def __init__(self, n_classes):
+    super(SentimentClassifier, self).__init__()
+    self.bert = BertModel.from_pretrained(PRE_TRAINED_MODEL_NAME)
+    self.drop = nn.Dropout(p=0.5)
+    self.out = nn.Linear(self.bert.config.hidden_size, n_classes)
+
+  def forward(self, input_ids, attention_mask):
+    
+    bert_output = self.bert(
+      input_ids=input_ids,
+      attention_mask=attention_mask,
+      return_dict=False
+    )
+    last_hidden_state, pooled_output = bert_output
+    output = self.drop(pooled_output)
+    return self.out(output)
+
+
+df_train, df_val = train_test_split(
+  df,
+  test_size=0.2,
+  random_state=RANDOM_SEED
+)
+
+train_data_loader = create_data_loader(df_train, tokenizer, MAX_LEN, BATCH_SIZE)
+val_data_loader = create_data_loader(df_val, tokenizer, MAX_LEN, BATCH_SIZE)
+data = next(iter(train_data_loader))
+model = SentimentClassifier(len(class_names))
+model = model.to(device)
+
+optimizer = AdamW(model.parameters(), lr=2e-5, correct_bias=False)
+total_steps = len(train_data_loader) * EPOCHS
+scheduler = get_linear_schedule_with_warmup(
+  optimizer,
+  num_warmup_steps=10,
+  num_training_steps=total_steps
+)
+loss_fn = nn.CrossEntropyLoss().to(device)
+
+def train_epoch(
+  model,
+  data_loader,
+  loss_fn,
+  optimizer,
+  device,
+  scheduler,
+  n_examples
+):
+
+  model = model.train()
+  losses = []
+  correct_predictions = 0
+  
+  for d in tqdm(data_loader):
+    input_ids = d["input_ids"].to(device)
+    attention_mask = d["attention_mask"].to(device)
+    targets = d["targets"].to(device)
+    outputs = model(
+      input_ids=input_ids,
+      attention_mask=attention_mask
+    )
+    _, preds = torch.max(outputs, dim=1)
+    loss = loss_fn(outputs, targets)
+    correct_predictions += torch.sum(preds == targets)
+    losses.append(loss.item())
+    loss.backward()
+    nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+    optimizer.step()
+    scheduler.step()
+    optimizer.zero_grad()
+  return correct_predictions.double() / n_examples, np.mean(losses)
+
+def eval_model(model, data_loader, loss_fn, device, n_examples):
+  model = model.eval()
+  losses = []
+  correct_predictions = 0
+  with torch.no_grad():
+    for d in tqdm(data_loader):
+      input_ids = d["input_ids"].to(device)
+      attention_mask = d["attention_mask"].to(device)
+      targets = d["targets"].to(device)
+      outputs = model(
+        input_ids=input_ids,
+        attention_mask=attention_mask
+      )
+      _, preds = torch.max(outputs, dim=1)
+      loss = loss_fn(outputs, targets)
+      correct_predictions += torch.sum(preds == targets)
+      losses.append(loss.item())
+  return correct_predictions.double() / n_examples, np.mean(losses)
+  
+
+history = defaultdict(list)
+best_accuracy = 0
+for epoch in range(EPOCHS):
+  print(f'Epoch {epoch + 1}/{EPOCHS}')
+  print('-' * 10)
+  train_acc, train_loss = train_epoch(
+    model,
+    train_data_loader,
+    loss_fn,
+    optimizer,
+    device,
+    scheduler,
+    len(df_train)
+  )
+  print(f'Train loss {train_loss} accuracy {train_acc}')
+  val_acc, val_loss = eval_model(
+    model,
+    val_data_loader,
+    loss_fn,
+    device,
+    len(df_val)
+  )
+  print(f'Val   loss {val_loss} accuracy {val_acc}')
+  print()
+  history['train_acc'].append(train_acc)
+  history['train_loss'].append(train_loss)
+  history['val_acc'].append(val_acc)
+  history['val_loss'].append(val_loss)
+  if val_acc > best_accuracy:
+    torch.save(model.state_dict(), 'best_model_state.bin')
+    best_accuracy = val_acc
+    
